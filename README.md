5c31079227b1378bf4ec75c08f085297839fe875
Anton Bakker <anton.bakker@kadaster.nl>
2019-12-03 22:02:48 +0100

    package code in python package

--- README.md
+++ README.md
@@ -6,18 +6,18 @@ CLI applicatie genereert metadata en voert schema validatie uit. Applicatie voer
 
 ## Gebruik
 
-Installeer eerst de dependencies (getest op Ubuntu in Bash shell):
+Installeer ngr-metadata-generator als pip package (uitvoeren vanuit root van repository):
 
 ```
-pip3 install -r requirements.txt
+pip3 install .
 ```
 
-Voor help:
+Nu moet het cli command `generate-metadata`/`gen-md` beschikbaar zijn het `PATH`:
 
 ```
-./generate_md.py --help
-Usage: generate_md.py [OPTIONS] VALUES_JSON_PATH
-                      [CSW|WMS|WMTS|WFS|WCS|SOS|ATOM]
+generate-metadata --help
+Usage: generate-metadata [OPTIONS] VALUES_JSON_PATH
+                         [CSW|WMS|WMTS|WFS|WCS|SOS|ATOM]
 
   Generate metadata record.
 
@@ -29,42 +29,5 @@ Options:
 Bijvoorbeeld (uitvoeren in root directory van dit repository):
 
 ```
-mkdir output && ./generate_md.py example_json/protectedsites_cdda_harmonized.json WMS --output-dir output/
+gen-md example_json/protectedsites_cdda_harmonized.json WMS --output-dir output/
 ```
-
-
-
-## Keuzes service metadata
-
-- temporalExtent: negeren  element, als dit relevant is dan moet het in de dataset metadata beschreven worden
-- securityConstraints: negeren element, als dit relevant is dan moet het in de dataset metadata beschreven worden
-
-
-## Vragen licentie
-
-- is waarde uit codelist verplicht? https://docs.geostandaarden.nl/md/mdprofiel-iso19119/#codelist-datalicenties
-- moet de waarde in het service-metadata record de waarde uit de dataset record overnemen wat betreft de data licenties?
-- geldt dit ook voor de uselimitations?
-- hoe zit het met licenties met naamsvermelding, moet PDOK daar als service provider de organisatie naam van data provider invullen? of die van pdok... (eerste lijkt me logischer)
-
-## Vragen rol contact
-
-- metadata contact, pdok gebruikt nu waarde pointOfContact > misschien beter resourceProvider (we verstrekken de metadata nl)
-- service contact, pdok gebruikt nu waarde author > misschien beter publisher of distributor?
-
-resourceProvider 	Partij die de data verstrekt.
-custodian 	Partij verantwoordelijk voor het beheer van de data.
-owner 	Partij die eigenaar is van de data.
-user 	Partij die de data gebruikt.
-distributor 	Partij die de data verstrekt.
-originator 	Partij die de data heeft gecreëerd
-pointOfContact 	Partij die optreedt als contactpunt voor uitwisselen van kennis of verstrekking van de data.
-principalInvestigator 	Partij die betrokken was bij de uitvoering van onderzoek.
-processor 	Partij die de data heeft bewerkt, zodanig dat de data is gewijzigd.
-publisher 	Partij die de data publiceert.
-author 	Partij die auteur is van de data.
-
-
-## Vragen MD_ClassificationCode 
-
-wel codelijst opgenomen https://docs.geostandaarden.nl/md/mdprofiel-iso19119/#codelist-classificationcode maar nergens gerefereerd, heeft het zin dit element op te nemen in PDOK service metadata?
--- metadata_generator/cli.py
+++ metadata_generator/cli.py
@@ -7,14 +7,15 @@ from datetime import datetime
 import click
 from lxml import etree
 from jinja2 import Environment, PackageLoader, select_autoescape, FileSystemLoader
+import pkg_resources
 
 SERVICE_TEMPLATES = {
-    "inspire":"iso19119_nl_profile_2.0_template_inspire_harmonized.xml",
-    "noninspire":"iso19119_nl_profile_2.0_regular.xml",
+    "inspire":"data/iso19119_nl_profile_2.0_template_inspire_harmonized.xml",
+    "noninspire":"data/iso19119_nl_profile_2.0_regular.xml",
 }
-CODELIST_JSON_FILE = "json/codelists.json"
+CODELIST_JSON_FILE = "data/json/codelists.json"
 
-SERVICE_TYPES =  ['CSW', 'WMS', 'WMTS', 'WFS', 'WCS', 'SOS', 'ATOM', 'TMS']
+SERVICE_TYPES = ['CSW', 'WMS', 'WMTS', 'WFS', 'WCS', 'SOS', 'ATOM', 'TMS']
 SERVICE_TYPES_CLI = ['CSW', 'WMS', 'WMTS', 'WFS', 'WCS', 'SOS', 'ATOM', 'TMS', 'IN_JSON']
 
 def clean_service_cap_url(url, service_type):
@@ -28,7 +29,8 @@ def clean_service_cap_url(url, service_type):
 def get_inspire_theme_label(data_json):
     inspire_mode = data_json["inspire"]
     if inspire_mode:
-        with open(CODELIST_JSON_FILE, 'r') as json_file:
+        json_path = pkg_resources.resource_filename(__name__, CODELIST_JSON_FILE)
+        with open(json_path, 'r') as json_file:
             codelists_json = json.loads(json_file.read())
             inspire_themes_codelist = codelists_json['inspire_themes']
             inspire_theme_uri = data_json['inspire_theme_uri']
@@ -39,7 +41,8 @@ def get_inspire_theme_label(data_json):
 
 def get_inspire_fields_by_service_protocol(service_protocol):
     # "inspire_service_type": "view",
-    with open(CODELIST_JSON_FILE, 'r') as json_file:
+    json_path = pkg_resources.resource_filename(__name__, CODELIST_JSON_FILE)
+    with open(json_path, 'r') as json_file:
         codelists_json = json.loads(json_file.read())
         inspire_servicetypes_codelist = codelists_json["codelist_inspire_service_types"]
         for item in inspire_servicetypes_codelist:
@@ -50,7 +53,8 @@ def get_inspire_fields_by_service_protocol(service_protocol):
 def get_service_protocol_values(service_type):
     # "service_protocol_name": "OGC:WMS",
     # "service_protocol_url": "http://www.opengeospatial.org/standards/wms"
-    with open(CODELIST_JSON_FILE, 'r') as json_file:
+    json_path = pkg_resources.resource_filename(__name__, CODELIST_JSON_FILE)
+    with open(json_path, 'r') as json_file:
         codelists_json = json.loads(json_file.read())
         inspire_servicetypes_codelist = codelists_json["codelist_protocol"]
         return inspire_servicetypes_codelist[service_type]
@@ -61,7 +65,7 @@ def get_service_template(data_json):
         result = SERVICE_TEMPLATES["inspire"]
     else:
         result = SERVICE_TEMPLATES["noninspire"]
-    return result
+    return pkg_resources.resource_filename(__name__, result)
 
 def get_service_url(data_json, service_type):
     # for now do not generate urls dynamically but use fallback 
@@ -98,15 +102,15 @@ def add_dynamic_fields(data_json, service_type):
         data_json["inspire_theme_label"] = inspire_theme_label
     return data_json
 
-def render_template(template_path, data_json, partials_path=""):
-    env = Environment(loader=FileSystemLoader('templates'),
+def render_template(template_path, data_json):
+    env = Environment(loader=FileSystemLoader(''),
         autoescape=select_autoescape(['xml']))
-    template = env.get_template(template_path)    
+    template = env.get_template(template_path)
     result = template.render(data_json)
     return result
 
 def get_md_identifier(json_path):
-     with open(json_path, 'r') as json_file:
+    with open(json_path, 'r') as json_file:
         config_json = json.loads(json_file.read())
         return config_json["md_identifier"]
 
@@ -120,7 +124,7 @@ def generate_service_metadata(json_path, service_type):
         config_json = json.loads(json_file.read())
         service_template = get_service_template(config_json)
         config_json = add_dynamic_fields(config_json, service_type)
-        md_record = render_template(service_template, config_json, "templates/partials")
+        md_record = render_template(service_template, config_json)
         validation_result = validate_service_metadata(md_record)
         if validation_result:
             print(f"metadata-generator error: generated metadata is invalid, validation message: {validation_result}")
@@ -145,7 +149,7 @@ def validate_service_metadata(xml_string):
     result = validate_xml_form(xml_string)
     if result:
         return result
-    schema_path = "schema/gmd_gmx.xsd"
+    schema_path = pkg_resources.resource_filename(__name__, "data/schema/gmd_gmx.xsd")
     with open(schema_path, 'rb') as xml_schema_file:
         schema_doc = etree.XML(xml_schema_file.read(), base_url=schema_path)
         schema = etree.XMLSchema(schema_doc)
